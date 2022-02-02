11106cec47477033f911abfa08b2457a482482a3
sekasara <63733959+sekasara@users.noreply.github.com>
2022-02-02 18:06:32 +0530

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,5 @@
+@Grapes([
+    @Grab (group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.1'),
+    @Grab (group='javax.xml.bind', module='jaxb-api', version='2.3.1'),
+    @Grab (group = 'com.veracode.vosp.api.wrappers', module = 'vosp-api-wrappers-java', version = '21.2.7.4')
+])
