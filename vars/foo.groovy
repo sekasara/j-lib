e94a05f15e4c5d53524b2c403139261851dec40f
sekasara <63733959+sekasara@users.noreply.github.com>
2022-02-02 18:11:25 +0530

    Create foo.groovy

---
+++ vars/foo.groovy
@@ -0,0 +1,10 @@
+@Grapes([
+    @Grab (group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7.1'),
+    @Grab (group='javax.xml.bind', module='jaxb-api', version='2.3.1'),
+    @Grab (group = 'com.veracode.vosp.api.wrappers', module = 'vosp-api-wrappers-java', version = '21.2.7.4')
+])
+
+import com.veracode.apiwrapper.wrappers.*
+import groovy.json.JsonOutput
+import groovy.xml.XmlSlurper
+import groovyx.net.http.RESTClient
